ba77dd5014a007b039ac679835febe79bce06bdb
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-09-27 15:11:35 +0100

    Add: Hash content for generated JSON files

--- fetch_data.py
+++ fetch_data.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import datetime 
+import hashlib
 from json import dump
 import os
 
@@ -25,12 +26,21 @@ def dump_json():
   #standardise file names 
   name = "data"  
   current_datetime = datetime.datetime.now().strftime("%y%m%d%H%M")
-  filename = f"json_dump/{name}_{current_datetime}.json"
+  
+  # generate a hash object for the name variable
+  hash_object = hashlib.md5(name.encode())
+  # get the hexadecimal representation of the hash and take the first 6 characters
+  hash_id = hash_object.hexdigest()[:6]
 
+  # construct the filename using the standardized format
+  filename = f"json_dump/{name}_{current_datetime}_{hash_id}.json"
+  
   # dump the data to the file, ensuring non-ASCII characters are preserved
   with open(filename, 'w') as f:
     dump(get_data(), f, ensure_ascii=False)
   f.close()
+
+   
 
 def get_dataframe():
 
