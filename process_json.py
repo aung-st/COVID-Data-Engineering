ce379cdac4ccc6f76cde628310c26496917d9836
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-09-28 17:04:08 +0100

    Add: create_key which combines a 3 character hash with a country name of an entry in the covid data

--- process_json.py
+++ process_json.py
@@ -1,6 +1,48 @@
 from fetch_data import get_data
+from generate_hash import generate_hash
+
 
 def load_json():
+
     # load and return json data
     return get_data()
+    
+def create_key(
+    country,
+):
+       
+    # generate a 3-character hash key
+    return generate_hash(4)[:3]+'-'+country
 
+def prepare_json(data):
+
+    for i in range(1):
+
+        continent = data['response'][i]['continent']
+        country = data['response'][i]['country']
+        population = data['response'][i]['population']
+        new_cases = data['response'][i]['cases']['new']
+        active_cases = data['response'][i]['cases']['active']
+        critical_cases = data['response'][i]['cases']['critical']
+        recovered = data['response'][i]['cases']['recovered']
+        recovered_1m_pop = data['response'][i]['cases']['1M_pop']
+        recovered_total = data['response'][i]['cases']['total']
+        new_deaths = data['response'][i]['deaths']['new']
+        deaths_1m_pop = data['response'][i]['deaths']['1M_pop']
+        deaths_total = data['response'][i]['deaths']['total']
+        tests_1m_pop = data['response'][i]['tests']['1M_pop']
+        tests_total = data['response'][i]['tests']['total']
+        day = data['response'][i]['day']
+        time = data['response'][i]['time']
+
+        id = create_key(country)
+        print(id)
+
+
+
+
+if __name__ == "__main__":
+    prepare_json(get_data())
+    
+
+    
