502029302df4c219dcaebd3bf9593dbfd535fa8a
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-09-28 13:39:46 +0100

    Amend: generate_hash function now generates arbitrary length hashes

--- generate_hash.py
+++ generate_hash.py
@@ -1,9 +1,10 @@
 import hashlib
 import os 
-def generate_hash():
+
+def generate_hash(length):
 
     # generate a hash object for a randomly generated string
     hash_object = hashlib.sha1()
-    hash_object.update(str(os.urandom(3)).encode())
+    hash_object.update(str(os.urandom(length)).encode())
     hash_id = hash_object.hexdigest()
     return hash_id
