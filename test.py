cf17158ce2eb47a2291daf1db57a88f5df707bde
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-10-02 00:03:33 +0100

    Add: test module as an entry point for project

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-from process_json import process_json, get_data
-from dump_json import dump_json
-dump_json()
+import database
+from double_check_jsons import check_json_is_inserted
+from fetch_data import get_data
+from process_json import process_json
+
+if __name__ == "__main__":    
+    check_json_is_inserted()
+    
