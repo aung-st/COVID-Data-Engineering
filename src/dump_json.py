bc0211ecda52312b735cbbfe513867c093bd13e2
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-10-21 00:27:40 +0100

    Amend: split dump_json function so that one function returns a filename and hash id and the other saves the json somewhere

--- src/dump_json.py
+++ src/dump_json.py
@@ -5,37 +5,39 @@ import sys
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname("src/")))) 
 from src.generate_hash import create_hash
 
+def create_file(path):
+    # standardise file names 
+    name = "data"  
+
+    # time of json dump
+    current_datetime = datetime.datetime.now().strftime("%y%m%d%H%M")
+    
+    
+    hash_id = create_hash(3)[:3]
+    
+    # construct the filename using the standardized format
+    filename = f"{path}{current_datetime}_{name}_{hash_id}.json"
+    
+    # prevent conflicts in json dumps
+    if os.path.exists(filename):
+      print("Potential conflict detected")
+
+      # reconstruct the filename with one hash character sliced off 
+      hash_id = create_hash(3)[:3]
+      filename = f"{path}{hash_id}_{name}_{current_datetime}.json"
+    
+    # to commit: return hash id for use in double check module
+    return filename,hash_id
   
+
 def dump_json(raw_json,path):
 
-  # standardise file names 
-  name = "data"  
-
-  # time of json dump
-  current_datetime = datetime.datetime.now().strftime("%y%m%d%H%M")
+  filename,_ = create_file(path)
   
-  
-  hash_id = create_hash(3)[:3]
-  
-  # construct the filename using the standardized format
-  filename = f"{path}{current_datetime}_{name}_{hash_id}.json"
-  
-  # prevent conflicts in json dumps
-  if os.path.exists(filename):
-    print("Potential conflict detected")
-
-    # reconstruct the filename with one hash character sliced off 
-    hash_id = create_hash(3)[:3]
-    filename = f"{path}{hash_id}_{name}_{current_datetime}.json"
-
-
-
   # dump the data to the file, ensuring non-ASCII characters are preserved
   with open(filename, 'w') as f:
     dump(raw_json, f, ensure_ascii=False)
   f.close()
 
-  # to commit: return hash id for use in double check module
-  return hash_id
-  
+ 
  
