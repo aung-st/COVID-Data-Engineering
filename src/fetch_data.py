f33bd595dc7b4d6df5321f3628d5addb67dfa8c6
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-10-21 13:04:15 +0100

    Add: history endpoint get function

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -17,6 +17,30 @@ def get_data():
   # return response data
   return data
 
+def get_history(country,day):
+
+  # get the RapidAPI key from the environment variable
+  rapid_api_key = os.getenv("RAPID_API_KEY")
+
+  # set the parameters for the API request
+  params = {
+    'country': country,
+    'day': day
+  }
+  # set the headers for the API request
+  headers = {'X-RapidAPI-Key': rapid_api_key,
+             'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'}
+
+  # make the API request
+  response = requests.get("https://covid-193.p.rapidapi.com/history",params = params,headers=headers)
+  data = response.json()
+
+  # return response data
+  return data
+
+
+
+
 
 
 
