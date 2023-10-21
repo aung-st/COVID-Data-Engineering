5b29e20704da0c6ab0a8f129dfc02eab6f5915a3
Sam Aung <92190177+aung-st@users.noreply.github.com>
2023-10-21 13:47:04 +0100

    Add: docstrings and type hinting to create hash

--- src/generate_hash.py
+++ src/generate_hash.py
@@ -1,10 +1,21 @@
 import hashlib
 import os 
 
-def create_hash(length):
+def create_hash(length:int) -> str:
+    
+    """
+    Create a hash id by encoding a string of random bytes of size length.
+
+    Parameters:
+    length (int): size of random bytes
+
+    Returns:
+    A hash id of length 40
+    """
 
     # generate a hash object for a randomly generated string
     hash_object = hashlib.sha1()
     hash_object.update(str(os.urandom(length)).encode())
     hash_id = hash_object.hexdigest()
     return hash_id
+
